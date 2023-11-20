abb23eb306dfa91ebb85a5633c5761c002d43ede
jmblx <zhora.zhilin.06@mail.ru>
2023-11-20 18:07:00 +0300

    added tasks 9, 10 and report 5

--- PZ_5/PZ_5.2.py
+++ PZ_5/PZ_5.2.py
@@ -9,9 +9,9 @@
 
 def SortInc3(A: float, B: float, C: float) -> float:
     arr = [A, B, C]
-    sorted_arr = []
+    sorted_arr = []  # Создание отсортированного массива
     num = float('infinity')
-    for i in reversed(range(3)):
+    for i in reversed(range(3)):  # Наполнение отсортированного массива
         for y in range(i+1):
             if arr[y] < num:
                 index = y
@@ -20,12 +20,16 @@ def SortInc3(A: float, B: float, C: float) -> float:
         arr.pop(index)
         num = float('infinity')
     return sorted_arr[0], sorted_arr[1], sorted_arr[2]
+    #  Возвращение элементов отсортированного массива
 
 
 def test_func(A: float, B: float, C: float) -> None:
+    print(f"до:\n{A}, {B}, {C}")
     A, B, C = SortInc3(A, B, C)
-    print(A, B, C)
+    print(f"после:\n{A}, {B}, {C}\n")
 
+
+# Тесты
 
 Ai, Bi, Ci = 4.1, 3.2, 5.6
 A2, B2, C2 = 9.4, 9.4, 2.3
